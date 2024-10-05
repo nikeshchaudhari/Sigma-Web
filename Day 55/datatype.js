4e00e8cfcbcdf12f34b3345a8e36033fbe1176de
Nikesh Chaudhar <info.nikesh12@gmail.com>
2024-10-06 00:18:09 +0545

    Sigma

---
+++ Day 55/datatype.js
@@ -0,0 +1,38 @@
+// primitive datatype
+
+//Null
+// Number
+// String
+// Symbol
+// Undefine
+// Boolean
+// Bigint
+
+// let x = "Nikesh"
+// let y = 55
+// let z  = 3.55
+// const p = true
+// let q = undefined
+// let r = null
+
+// console.log(x,y,z,p,q,r);
+// console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+// Object
+
+let o ={
+    name : "Nikesh",
+    "job role": "Developer"
+}
+console.log(o);
+o.salary = 50000;
+console.log(o);
+
+
+let student ={
+    name : "Rohit",
+    phone : 98888,
+    marks : 95
+
+}
+console.log(student);
---
+++ Day 55/index.js
@@ -0,0 +1,18 @@
+console.log("Sigma Web Development");
+
+let a = 3;
+let b = 5.0;
+let c = "Nikesh"
+
+ console.log(a+b+8);
+ console.log(typeof a, typeof b, typeof c);
+ {
+    let a = 66
+    console.log(a);
+    
+ }
+ console.log(a);
+ 
+ const a1 = 5 // not change
+
+
