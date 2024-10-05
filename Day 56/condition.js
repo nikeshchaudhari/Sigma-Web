80a1b872262a1d4d8c50a42d80c2074c275d00b9
Nikesh Chaudhar <info.nikesh12@gmail.com>
2024-10-06 00:38:04 +0545

    Sigma

---
+++ Day 56/condition.js
@@ -0,0 +1,29 @@
+console.log("Sigma Condition Statement");
+
+// Operator
+let age = 17;
+
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
+
+ age>18 ? "You are dived" : "Not drive"
+ console.log(age);
+ 
+
+// if((age+grace)>18){
+//     console.log("You can Drive");
+    
+// }
+// else if(age == 0){
+//     console.log("Are you kidding");
+    
+// }
+// else{
+// console.log("Cannot Drive");
+
+// }
